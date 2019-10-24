4f01f4ef92de950e4e91e1774f339e9fa873d180
Pedro Larroy <pedro.larroy.lists@gmail.com>
2019-10-23 17:40:10 -0700

    refactor

--- paquito.py
+++ paquito.py
@@ -40,25 +40,107 @@ def flatten(xs):
     return list(itertools.chain.from_iterable(xs))
 
 
+def _create_ami_image(
+    ec2: object,
+    instance_id: str,
+    image_name: str,
+    image_description: str,
+    launch_template: object) -> None:
+
+    logging.info("Creating AMI %s from instance %s...", image_name, instance_id)
+    create_image_args = dict(
+        BlockDeviceMappings = launch_template['CreateInstanceArgs']['BlockDeviceMappings']
+    )
+    create_image(ec2, instance_id, image_name, image_description, **create_image_args)
+    logging.info("AMI creationg complete.")
+
+
+def _provision(ec2_resource, ec2_client, launch_template, args) -> None:
+    try:
+        logging.info("Creating security groups")
+        security_groups = create_ssh_anywhere_sg(ec2_client, ec2_resource)
+    except botocore.exceptions.ClientError as e:
+        logging.info("Continuing: Security group might already exist or be used by a running instance")
+        res = ec2_client.describe_security_groups(GroupNames=['ssh_anywhere'])
+        security_groups = [res['SecurityGroups'][0]['GroupId']]
+
+
+    try:
+        ec2_client.import_key_pair(KeyName=args.ssh_key_name, PublicKeyMaterial=read_file(args.ssh_key_file))
+    except botocore.exceptions.ClientError as e:
+        logging.info("Continuing: Key pair '%s' might already exist", args.ssh_key_name)
+
+    aws_account = boto3.client('sts').get_caller_identity()['Account']
+    logging.info("""
+
+    AWS Account: %s
+    Instance type: %s
+    Region: %s
+    Base AMI: %s
+    SSH Key: %s (from: %s)
+    Security groups: %s
+    Playbook: %s
+    User Data: %s
+
+    """, aws_account, args.instance_type, boto3.session.Session().region_name, args.ami, args.ssh_key_name,
+         args.ssh_key_file, security_groups, args.playbook, args.user_data)
+
+
+    logging.info("creating instances")
+    instances = create_instances(
+        ec2_resource,
+        args.instance_name,
+        args.instance_type,
+        args.ssh_key_name,
+        args.ami,
+        security_groups,
+        args.user_data,
+        launch_template.get('CreateInstanceArgs', {}))
+    try:
+        wait_for_instances(instances)
+        for instance in instances:
+            host = instance.public_dns_name
+            logging.info("Waiting for host {}".format(host))
+            wait_port_open(host, 22, 300)
+            ansible_provision_host(host, args.username, args.playbook)
+            logging.info("All done, the following hosts are now available: %s", host)
+        instance = next(iter(instances))
+        logging.info("Imaging the first instance: %s", instance.instance_id)
+        _create_ami_image(ec2_client, instance.instance_id, args.image_name,
+                          args.image_description, launch_template)
+
+
+    finally:
+        if not args.keep_instance:
+            logging.info("Terminate instances")
+            instances.stop()
+
+
 def parse_args(**kwargs):
     parser = argparse.ArgumentParser(description="Paquito AMI packer")
     parser.add_argument('-n', '--instance-name', default=kwargs.get('instance-name',
                         "{}-{}".format('paquito', getpass.getuser())))
     parser.add_argument('-i', '--instance-type', default=kwargs.get('instance-type'))
-    parser.add_argument('--ubuntu', default=kwargs.get('ubuntu'))
+    parser.add_argument('--ubuntu', default=kwargs.get('ubuntu'),
+                        help="Specify an ubuntu release like 18.04 instead of an ami")
     parser.add_argument('-u', '--username',
                         default=kwargs.get('username', getpass.getuser()))
     ssh_key = kwargs.get('ssh-key', os.path.join(expanduser("~"),".ssh","id_rsa.pub"))
     parser.add_argument('--ssh-key-file', default=ssh_key)
     parser.add_argument('--ssh-key-name', default="ssh_{}_key".format(getpass.getuser()))
     parser.add_argument('-a', '--ami', default=kwargs.get('ami'))
-    parser.add_argument('-p', '--playbook', default=kwargs.get('playbook'))
+    parser.add_argument('-p', '--playbook', default=kwargs.get('playbook'),
+                        help="Ansible playbook to use for provisioning")
     parser.add_argument('-m', '--image-name', default=kwargs.get('image-name'))
-    parser.add_argument('--user-data', nargs="*")
+    parser.add_argument('--user-data', nargs="*",
+                        help="Add a flat list of file and mime type pair files to use for user data"
+                        "for the instance")
     parser.add_argument('--keep-instance',
                         help="Keep instance on to diagnose problems",
                         action='store_true')
     parser.add_argument('-d', '--image-description', default=kwargs.get('image-description'))
+    parser.add_argument('--image-instance-id',
+                        help="Image an existing instance by instance id")
     parser.add_argument('rest', nargs='*')
     args = parser.parse_args()
     return args
@@ -113,83 +195,23 @@ def main():
             launch_template = yaml.load(f, Loader=yaml.SafeLoader)
 
     args = parse_args(**launch_template)
+
     if args.user_data:
         args.user_data = group_user_data(args.user_data)
     else:
         args.user_data = launch_template['user-data']
 
-    boto3_session = boto3.session.Session()
-    current_region = boto3_session.region_name
-
-    fill_args_interactive(args, current_region)
+    fill_args_interactive(args, boto3.session.Session().region_name)
     validate_args(args)
 
     ec2_resource = boto3.resource('ec2')
     ec2_client = boto3.client('ec2')
-    aws_account = boto3.client('sts').get_caller_identity()['Account']
 
-
-    try:
-        logging.info("Creating security groups")
-        security_groups = create_ssh_anywhere_sg(ec2_client, ec2_resource)
-    except botocore.exceptions.ClientError as e:
-        logging.info("Continuing: Security group might already exist or be used by a running instance")
-        res = ec2_client.describe_security_groups(GroupNames=['ssh_anywhere'])
-        security_groups = [res['SecurityGroups'][0]['GroupId']]
-
-
-    try:
-        ec2_client.import_key_pair(KeyName=args.ssh_key_name, PublicKeyMaterial=read_file(args.ssh_key_file))
-    except botocore.exceptions.ClientError as e:
-        logging.info("Continuing: Key pair '%s' might already exist", args.ssh_key_name)
-
-    logging.info("""
-
-    AWS Account: %s
-    Instance type: %s
-    Region: %s
-    Base AMI: %s
-    SSH Key: %s (from: %s)
-    Security groups: %s
-    Playbook: %s
-    User Data: %s
-
-    """, aws_account, args.instance_type, current_region, args.ami, args.ssh_key_name,
-         args.ssh_key_file, security_groups, args.playbook, args.user_data)
-
-
-    logging.info("creating instances")
-    instances = create_instances(
-        ec2_resource,
-        args.instance_name,
-        args.instance_type,
-        args.ssh_key_name,
-        args.ami,
-        security_groups,
-        args.user_data,
-        launch_template.get('CreateInstanceArgs', {}))
-
-    wait_for_instances(instances)
-    hosts = [i.public_dns_name for i in instances]
-    try:
-        for host in hosts:
-            logging.info("Waiting for host {}".format(host))
-            wait_port_open(host, 22, 300)
-            ansible_provision_host(host, args.username, args.playbook)
-        logging.info("All done, the following hosts are now available: %s", hosts)
-
-
-        logging.info("Creating AMI %s from instance %s...", hosts[0], image_name)
-        create_image_args = dict(
-            BlockDeviceMappings = launch_template['CreateInstanceArgs']['BlockDeviceMappings']
-        )
-        create_image(ec2_client, hosts[0], image_name, image_description, **create_image_args)
-        logging.info("AMI creationg complete.")
-    finally:
-        if not args.keep_instance:
-            logging.info("Terminate instances")
-            for instance in instances:
-                instance.terminate()
+    if args.image_instance_id:
+        _create_ami_image(ec2_client, args.image_instance_id, args.image_name, args.image_description,
+        launch_template)
+    else:
+        _provision(ec2_resource, ec2_client, launch_template, args)
     return 0
 
 if __name__ == '__main__':
